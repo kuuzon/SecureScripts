46fd2e19fa191eb062fab26b7db4f5280346f281
Alex Bicknell <alexander.bicknell@gmail.com>
2020-06-12 15:10:51 +1000

    Doctor & Pharm routes coded

--- routes/doctors.js
+++ routes/doctors.js
@@ -13,16 +13,79 @@ router.get('/', async (req, res) => {
 });
 
 //[2] CREATE (post) Route
+router.post('/', async (req, res) => {
+    //Validation
+    const {error} = validateDoctor(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
+    //Create new doctor model + record post data
+    let doctor = new Doctor({
+        name: req.body.name,
+        clinicAddress: {
+            name: req.body.clinicAddress.name,
+            streetNumber: req.body.clinicAddress.streetNumber,
+            street: req.body.clinicAddress.street,
+            city: req.body.clinicAddress.city,
+            state: req.body.clinicAddress.state,
+            postcode: req.body.clinicAddress.postcode
+        },
+        email: req.body.email
+    });
+
+    //Save model to database & response to client
+    doctor = await doctor.save();
+    res.send(`The details for Dr ${doctor.name} have been successfully registered to the SecureScripts database.`)
+});
 
 //[3] READ (get) Route for Specific Doctor ID
-
+router.get('/:id', async (req, res) => {
+    const doctor = await Doctor.findById(req.params.id);
+    if(!doctor) {
+        res.status(404).send(`The doctor with an ID of ${req.params.id} does not exist.  Please ensure the doctor ID has been entered correctly.`)
+    } else {
+        res.send(doctor)
+    };
+});
 
 //[4] UPDATE (put) Route for Specific Doctor ID
+router.put('/:id', async (req, res) => {
+    //Validation
+    const {error} = validateDoctor(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
+    //Find ID and update data with body data
+    const doctor = await Doctor.findByIdAndUpdate(req.params.id, 
+        {
+            name: req.body.name,
+            clinicAddress: {
+                name: req.body.clinicAddress.name,
+                streetNumber: req.body.clinicAddress.streetNumber,
+                street: req.body.clinicAddress.street,
+                city: req.body.clinicAddress.city,
+                state: req.body.clinicAddress.state,
+                postcode: req.body.clinicAddress.postcode
+            },
+            email: req.body.email
+        }, {new: true});
+    
+    if(!doctor) {
+        res.status(404).send(`The doctor with an ID of ${req.params.id} does not exist.  Please ensure the doctor ID has been entered correctly.`)
+    } else {
+        res.send(`The details for Dr ${doctor.name} have been successfully updated!`)
+    };
+});
 
 //[5] DELETE (delete) Route for Specific Doctor ID
+router.delete('/:id', async (req, res) => {
+    //Find ID & run if/else statement based on ID in URL
+    const doctor = await Doctor.findOneAndRemove(req.params.id);
 
+    if(!doctor) {
+        res.status(404).send(`The doctor with an ID of ${req.params.id} does not exist.  Please ensure the doctor ID has been entered correctly.`)
+    } else {
+        res.send(`The doctor, Dr ${doctor.name} has been successfully removed from the SecureScripts database.`)
+    };
+});
 
 //Export module
 module.exports = router;
--- routes/pharms.js
+++ routes/pharms.js
@@ -13,16 +13,81 @@ router.get('/', async (req, res) => {
 });
 
 //[2] CREATE (post) Route
+router.post('/', async (req, res) => {
+    //Validation
+    const {error} = validatePharm(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
+    //Create new pharm model + record post data
+    let pharm = new Pharm({
+        name: req.body.name,
+        pharmAddress: {
+            name: req.body.pharmAddress.name,
+            streetNumber: req.body.pharmAddress.streetNumber,
+            street: req.body.pharmAddress.street,
+            city: req.body.pharmAddress.city,
+            state: req.body.pharmAddress.state,
+            postcode: req.body.pharmAddress.postcode
+        },
+        email: req.body.email 
+    });
+
+    //Save model to Database & response to client
+    pharm = await pharm.save();
+    res.send(`The details for ${pharm.name} have been successfully registered to the SecureScripts database.`)
+});
 
 //[3] READ (get) Route for Specific Doctor ID
-
+router.get('/:id', async (req, res) => {
+    const pharm = await Pharm.findById(req.params.id);
+    if (!pharm) {
+        res.status(404).send(`The pharmacy with an ID of ${req.params.id} does not exist.  Please ensure the pharmacy ID has been entered correctly.`)
+    } else {
+        res.send(pharm)
+    };
+});
 
 //[4] UPDATE (put) Route for Specific Doctor ID
+router.put('/:id', async (req, res) => {
+    //Validation
+    const {error} = validatePharm(req.body);
+    if (error) {
+        res.status(400).send(error.details[0].message);
+    } else {
+        //Find ID and update data with body data
+        const pharm = await Pharm.findByIdAndUpdate(req.params.id,
+            {
+                name: req.body.name,
+                pharmAddress: {
+                    name: req.body.pharmAddress.name,
+                    streetNumber: req.body.pharmAddress.streetNumber,
+                    street: req.body.pharmAddress.street,
+                    city: req.body.pharmAddress.city,
+                    state: req.body.pharmAddress.state,
+                    postcode: req.body.pharmAddress.postcode
+                },
+                email: req.body.email 
+            }, {new:true});
 
+        if(!pharm) {
+            res.status(404).send(`The pharmacy with an ID of ${req.params.id} does not exist.  Please ensure the pharmacy ID has been entered correctly.`)
+        } else {
+            res.send(`The details for ${pharm.name} have been successfully updated!`)
+        };
+    };
+});
 
 //[5] DELETE (delete) Route for Specific Doctor ID
+router.delete('/:id', async (req, res) => {
+    //Find ID & run if/else statement based on ID passed into URL
+    const pharm = await Pharm.findByIdAndRemove(req.params.id);
 
+    if(!pharm) {
+        res.status(404).send(`The pharmacy with an ID of ${req.params.id} does not exist.  Please ensure the pharmacy ID has been entered correctly.`)
+    } else {
+        res.send(`The details for ${pharm.name} has been successfully removed from the SecureScripts database.`)
+    };
+});
 
 //Export module
 module.exports = router;
